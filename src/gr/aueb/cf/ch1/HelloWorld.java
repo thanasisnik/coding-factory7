bb96e5f9c9fbb39208823d212a92de7aed979b0a
Thanasis Nikolopoulos <81475280+thanasisnik@users.noreply.github.com>
2024-12-10 20:01:30 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * doc comments
+ * java doc
+ * Reason is to understand what THIS CLASS is doing
+ * Prints Hello to Coding Factory in standard output (console).
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //Inline comments: prints hello coding factory
+        /*
+            Block Comments
+            prints hello coding factory
+         */
+        System.out.println("Hello to Coding Factory!");
+    }
+
+}
